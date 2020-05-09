8f00872f2d6a619f73df89f8feb52b94e085c04b
dasad44 <dasad33_79@o2.pl>
2020-05-09 17:33:47 +0200

    dsa

---
+++ ConsoleApp3/ConsoleApp3/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Class1
+    {
+        private void SaveMultiClipboard()
+        {
+            if (Keyboard.IsKeyDown(Key.C)           //ctrl+C
+          && Keyboard.IsKeyDown(Key.LeftCtrl) && ctrl1clicked == false && ctrl2clicked == false
+          || Keyboard.IsKeyDown(Key.RightCtrl)
+          && Keyboard.IsKeyDown(Key.C) && ctrl1clicked == false && ctrl2clicked == false)
+            {
+                tmp1 = Clipboard.GetText();
+                if (tmp1 == null || tmp1 == text_2 || tmp1 == text_3 || tmp1 == text_1)
+                {
+                    Clipboard.SetDataObject(text_1);
+                }
+                else
+                {
+                    text_3 = text_2;
+                    text_2 = text_1;
+                    text_1 = clipboardhandler.SaveText();
+                }
+                ctrl1clicked = true;
+            }
+            else if (Keyboard.IsKeyUp(Key.C)
+          && Keyboard.IsKeyUp(Key.LeftCtrl)
+          || Keyboard.IsKeyUp(Key.RightCtrl)
+          && Keyboard.IsKeyUp(Key.C))
+            {
+                ctrl1clicked = false;
+                ctrl2clicked = false;
+            }
+            else if (Keyboard.IsKeyDown(Key.C)           //ctrl+CC
+          && Keyboard.IsKeyDown(Key.LeftCtrl) && ctrl1clicked == true && ctrl2clicked == false
+          || Keyboard.IsKeyDown(Key.RightCtrl)
+          && Keyboard.IsKeyDown(Key.C) && ctrl1clicked == true && ctrl2clicked == false)
+            {
+                //ctrl1clicked = false;
+                //text_1 = clipboardhandler.SaveText();
+                ctrl2clicked = true;
+                Clipboard.SetDataObject(text_2);
+                //MessageBox.Show("D1111");
+            }
+            else if (Keyboard.IsKeyUp(Key.C)
+          && Keyboard.IsKeyUp(Key.LeftCtrl)
+          || Keyboard.IsKeyUp(Key.RightCtrl)
+          && Keyboard.IsKeyUp(Key.C))
+            {
+                ctrl1clicked = false;
+                ctrl2clicked = false;
+            }
+            else if (Keyboard.IsKeyDown(Key.C)                  //ctrl+CCC
+          && Keyboard.IsKeyDown(Key.LeftCtrl) && ctrl2clicked == true && ctrl2clicked == true
+          || Keyboard.IsKeyDown(Key.RightCtrl)
+          && Keyboard.IsKeyDown(Key.C) && ctrl2clicked == true && ctrl2clicked == true)
+            {
+                //text_1 = clipboardhandler.SaveText();
+                Clipboard.SetDataObject(text_3);
+                ctrl2clicked = false;
+                ctrl1clicked = false;
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string st = "";
+
+            NotifyIcon notyfyIcon = new NotifyIcon();
+            notyfyIcon.Visible = true;
+            notyfyIcon.Icon = SystemIcons.Application;
+            notyfyIcon.ShowBalloonTip(1000, "dsadas", "Daaaaaaaaaaaaaa", ToolTipIcon.Info);
+            notyfyIcon.Dispose();
+
+            Console.ReadKey();
+        }
+    }
+}
