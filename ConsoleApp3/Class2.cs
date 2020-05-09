e8aa755e5a165c527e138bb822d851c02a3b0483
dasad44 <dasad33_79@o2.pl>
2020-05-09 21:01:06 +0200

    dsadassa

---
+++ ConsoleApp3/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Class2
+    {
+        dsadsadsdsdsdsads
+            da
+            dsadsadsadsadsadsads
+            dsdasdsadsadsa
+            sd
+            as
+            decimalsd
+            ad
+            sd
+            a
+    }
+}
